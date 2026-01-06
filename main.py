75a111ba5faf77604b24fc4498bf939f753cb352
Bootleg Fish <hypegaming.4k@gmail.com>
2026-01-06 17:38:14 -0500

    Refactor Flask routes and add new endpoints

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 from flask import Flask, render_template
 import random
-from datetime import datetime
+import hash
+import sys
+import webbrowser
 from data import *
 
+def help():
+	webbrowser.open("https://github.com/thebtlgfish/fishapi/blob/main/README.md")
+
+
+
+p = sys.argv[1]
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api")
 def index():
 	return render_template("index.html")
 
-@app.route("/quote")
+@app.route("/api/random/quote")
 def quote():
 	return random.choice(quotes)
 
-@app.route("/name")
-def name():
-	return random.choice(names)
+@app.route("/api/random/girlname")
+def getgirlname():
+	return random.choice(girl_names)
 
-@app.route("/time")
-def time():
-	now = datetime.now()
-	current_time = now.strftime("%H:%M:%S")  
-	return current_time
+@app.route("/api/random/boyname")
+def getboyname():
+	return random.choice(boy_names)
+
+@app.route('/api/conversion/sha256/<plain_text>', methods=['GET'])
+def sha256(plain_text):
+	return hash.sha256gen(plain_text)
+
+@app.route("/api/random/joke")
+def getrandomjoke():
+	return random.choice(jokes)
 
 
 
@@ -29,5 +43,8 @@ def time():
 
 
 
+if p == "--help":
+	help()
+	sys.exit(0)
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=p)
